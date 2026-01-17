a3096593a2c3b899700aa44a00b63bbdafa4c8e8
TheegalaNitin <t.nitin2011@gmail.com>
2026-01-17 16:02:37 +0100

    Create SequenceFIles_TaxiIDCount.java

---
+++ SequenceFIles_TaxiIDCount.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat; // Import for SequenceFile
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class TaxiIDCount {
+
+    public static class TokenizerMapper
+            extends Mapper<Text, NullWritable, Text, IntWritable>{ 
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Text key, NullWritable value, Context context 
+        ) throws IOException, InterruptedException {
+            // Since we're reading from SequenceFiles, the 'key' parameter directly contains the line
+            // No need to split the line as before
+            context.write(key, one);
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: taxiidcount <in> <out>");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "taxi id count");
+        job.setJarByClass(TaxiIDCount.class);
+
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.setInputFormatClass(SequenceFileInputFormat.class); // Set input format to SequenceFileInputFormat
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
